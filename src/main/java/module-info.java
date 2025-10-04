ad8d15942709a5e02a9b665b76ccd839408c89e7
Gia Luong <luongduonggia@gmail.com>
2025-10-04 14:47:13 +0700

    delete all

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.pdm_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports com.example.pdm_project;                 // cho phép JavaFX khởi tạo LoginApp
-    opens   com.example.pdm_project to javafx.fxml;  // cho FXMLLoader truy cập controller
-}
