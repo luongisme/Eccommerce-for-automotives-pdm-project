bf2d975d388e547c5bd04b4ed0b1161a0266b674
Viet Ha <taolaha.hg@gmail.com>
2025-10-07 18:15:17 +0700

    Admin (create for Khoa)

---
+++ src/main/java/com/UI/admin/AdminDashboard.java
@@ -0,0 +1,12 @@
+package com.UI.admin;
+
+public class AdminDashboard {
+
+    public AdminDashboard() {
+        System.out.println("Logger initialized!");
+    }
+
+    public void setScreen() {
+        System.out.println("Admin Dashboard initialized!");
+    }
+}
